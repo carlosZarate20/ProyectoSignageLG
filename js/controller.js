17d4eac010490ad10181c98191d1681917c79779
zaratec31@hotmail.com <u201314894@upc.edu.pe>
2019-06-19 01:44:43 -0500

    Ultimos cambios de LG

--- js/controller.js
+++ js/controller.js
@@ -1434,9 +1434,9 @@
 
                  } else if (arreglo.respuesta == 2) {
                      $('#Agenda2').hide();
-                     $('#Agenda3').hide();
+                     // $('#Agenda3').hide();
                      $('#Agenda4').show();
-                     //$('#Agenda').show();
+                     $('#Agenda').show();
                      $('#Cuadro3Modificar').html('<label style="display:block;color: white; font-family: b-medium; font-size: 17px; display: inline-block;">"Ver Agenda"</label>')  
 
                  } else {
